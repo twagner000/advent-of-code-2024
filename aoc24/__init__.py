5576280e308b3cad6e1ad5ca2c3829274ec0a1fa
Tristan Wagner <twagner000@users.noreply.github.com>
2024-12-01 21:41:45 -0800

    reorg examples

--- aoc24/__init__.py
+++ aoc24/__init__.py
@@ -3,3 +3,4 @@
 # ruff: noqa: F401
 from .p01 import p01a, p01b
 from .p02 import p02a, p02b
+from .p03 import p03a, p03b
--- tests/test_aoc24.py
+++ tests/test_aoc24.py
@@ -10,15 +10,16 @@ DATA_PATH = Path("data")
 
 with Path("tests/examples.yaml").open() as f:
     EXAMPLES = yaml.safe_load(f)
+EXAMPLE_KEYS = [(k, k2) for k, v in EXAMPLES.items() for k2 in v if k2 != "input"]
 
 
-@pytest.mark.parametrize("puzzle", EXAMPLES.keys())
-def test_aoc24(puzzle: str):
-    puzzle_func = getattr(aoc24, puzzle)
+@pytest.mark.parametrize(("puzzle", "part"), EXAMPLE_KEYS)
+def test_aoc24(puzzle: str, part: str):
+    puzzle_func = getattr(aoc24, puzzle + part)
     with StringIO() as example_input:
         example_input.write(EXAMPLES[puzzle]["input"])
         example_input.seek(0)
-        assert puzzle_func(example_input) == EXAMPLES[puzzle]["solution"]
-    if not EXAMPLES[puzzle].get("completed"):
-        full_solution = f"The full solution is {puzzle_func(DATA_PATH / f'{puzzle[:3]}.txt')}"
+        assert puzzle_func(example_input) == EXAMPLES[puzzle][part]["solution"]
+    if not EXAMPLES[puzzle][part].get("completed"):
+        full_solution = f"The full solution is {puzzle_func(DATA_PATH / 'puzzle.txt')}"
         raise Exception(full_solution)
