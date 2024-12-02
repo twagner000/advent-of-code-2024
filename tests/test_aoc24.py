9f643cad6e9bebd21942aab9aaa45331d45155dc
Tristan Wagner <twagner000@users.noreply.github.com>
2024-12-01 21:55:30 -0800

    simplify examples

--- tests/test_aoc24.py
+++ tests/test_aoc24.py
@@ -10,7 +10,7 @@ DATA_PATH = Path("data")
 
 with Path("tests/examples.yaml").open() as f:
     EXAMPLES = yaml.safe_load(f)
-EXAMPLE_KEYS = [(k, k2) for k, v in EXAMPLES.items() for k2 in v if k2 != "input"]
+EXAMPLE_KEYS = [(k, k2) for k, v in EXAMPLES.items() for k2 in v if len(k2) == 1]
 
 
 @pytest.mark.parametrize(("puzzle", "part"), EXAMPLE_KEYS)
@@ -19,7 +19,8 @@ def test_aoc24(puzzle: str, part: str):
     with StringIO() as example_input:
         example_input.write(EXAMPLES[puzzle]["input"])
         example_input.seek(0)
-        assert puzzle_func(example_input) == EXAMPLES[puzzle][part]["solution"]
-    if not EXAMPLES[puzzle][part].get("completed"):
-        full_solution = f"The full solution is {puzzle_func(DATA_PATH / 'puzzle.txt')}"
+        assert puzzle_func(example_input) == EXAMPLES[puzzle][part]
+    if EXAMPLES[puzzle].get("print_full_solutions"):
+        puzzle_input_path = DATA_PATH / f"{puzzle}.txt"
+        full_solution = f"{puzzle_func(puzzle_input_path)} is the full solution."
         raise Exception(full_solution)
