fe6ddae112527a6404d2737e4f008df655d9afce
Tristan Wagner <twagner000@users.noreply.github.com>
2024-12-01 17:00:43 -0800

    use completed=False to output full result

--- aoc24/__init__.py
+++ aoc24/__init__.py
@@ -1,5 +1,5 @@
 """Advent of Code 2024 solutions."""
 
-from .d01 import p01a, p01b
+from .p01 import p01a, p01b
 
 __all__ = ["p01a", "p01b"]
--- tests/test_aoc24.py
+++ tests/test_aoc24.py
@@ -6,6 +6,8 @@ import yaml
 
 import aoc24
 
+DATA_PATH = Path("data")
+
 with Path("tests/examples.yaml").open() as f:
     EXAMPLES = yaml.safe_load(f)
 
@@ -17,6 +19,6 @@ def test_aoc24(puzzle: str):
         example_input.write(EXAMPLES[puzzle]["input"])
         example_input.seek(0)
         assert puzzle_func(example_input) == EXAMPLES[puzzle]["solution"]
-    if fpath := EXAMPLES[puzzle].get("full_problem"):
-        full_solution = f"The full solution is {puzzle_func(fpath)}"
+    if not EXAMPLES[puzzle].get("completed"):
+        full_solution = f"The full solution is {puzzle_func(DATA_PATH / f'{puzzle[:3]}.txt')}"
         raise Exception(full_solution)
