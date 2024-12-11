5308b1e0be3d29fd68dbb15ebcef44b53755a040
Tristan Wagner <twagner000@users.noreply.github.com>
2024-12-10 20:59:24 -0800

    comments

--- aoc24/p09.py
+++ aoc24/p09.py
@@ -33,17 +33,19 @@ def p09b(input_stream: TextIOBase) -> int:
     blocks = [(i // 2 if i % 2 == 0 else None, int(x)) for i, x in enumerate(disk_map)]
     for file in blocks[-1::-2]:
         src_i = blocks.index(file)  # index of file we're trying to move
-        dest_i = 0  # index of file/space we're trying to move the file into
+        dest_i = 0  # index of space we're trying to move the file into
         while (dest := blocks[dest_i]) != file:  # stop once we've hit our file
             if dest[0] is None and dest[1] >= file[1]:
-                blocks[src_i] = (None, file[1])  # replace the original file location with empty space
+                blocks[src_i] = (None, file[1])
                 blocks[dest_i] = file
                 if (remainder := dest[1] - file[1]) > 0:
                     # if our file was smaller than the empty space, we need to keep the remainder
                     if blocks[dest_i + 1][0] is None:
-                        blocks[dest_i + 1] = (None, blocks[dest_i + 1][1] + remainder)  # merge into following space
+                        # add remainder to following space
+                        blocks[dest_i + 1] = (None, blocks[dest_i + 1][1] + remainder)
                     else:
-                        blocks.insert(dest_i + 1, (None, remainder))  # add new block with remainder
+                        # create a new space for the remainder
+                        blocks.insert(dest_i + 1, (None, remainder))
                 break
             dest_i += 1
 
