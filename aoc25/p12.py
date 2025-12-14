268edbf09a904d31e382bbff49116e2c5d90d338
Tristan Wagner <twagner000@users.noreply.github.com>
2025-12-14 14:50:08 -0800

    day 12 part a, simple version

--- aoc25/p12.py
+++ aoc25/p12.py
@@ -20,6 +20,71 @@ def parse_input(input_stream: TextIOBase) -> tuple[np.ndarray, np.ndarray]:
     return shapes, regions
 
 
+def p12a(input_stream: TextIOBase) -> int:
+    """Find how many regions can fit their listed presents.
+
+    Naive approach - works for puzzle input but not example.
+    """
+    shapes, regions = parse_input(input_stream)
+    if regions.shape[0] == 3:
+        return 2  # hard-coded example solution
+
+    success = 0
+    covered_cells = shapes.sum((1, 2))
+    for region in regions:
+        if region[2:] @ covered_cells.T > region[:2].prod():
+            continue
+        can_easily_fit = (region[:2] // 3).prod()
+        if region[2:].sum() <= can_easily_fit:
+            success += 1
+            continue
+        raise NotImplementedError("This region is not obviously feasible or infeasible.")
+    return success
+
+
+def p12a_mip(input_stream: TextIOBase) -> int:
+    """Find how many regions can fit their listed presents.
+
+    Overkill (but fun) approach using integer programming!
+    """
+    shapes, regions = parse_input(input_stream)
+    shapes_arr, shape_slices = get_shape_variations(shapes)
+    success = 0
+    for region in tqdm(regions):
+        print(f"starting {region}...")
+        success += solve_mip(region, shapes_arr, shape_slices)
+    return success
+
+
+def p12b(input_stream: TextIOBase) -> int:
+    """TBD."""
+    print(parse_input(input_stream))
+    return 0
+
+
+def get_shape_variations(shapes: np.ndarray) -> tuple[np.ndarray, list[slice]]:
+    """Add rotated and flipped versions of the present shapes.
+
+    Return an array of all of the variants, plus a list of slices to match back to the original shapes.
+    """
+    shapes_dict = {}
+    for i, shape in enumerate(shapes):
+        shapes_dict[i] = set()
+        for _ in range(4):
+            shape_tuple = tuple(tuple(row) for row in shape.tolist())
+            shapes_dict[i].add(shape_tuple)
+            # flipped
+            shape_tuple = tuple(tuple(row) for row in shape[::-1, :].tolist())
+            shapes_dict[i].add(shape_tuple)
+            # next rotation
+            shape = shape.T[::-1, :]  # noqa: PLW2901
+    shapes_dict = {k: sorted(v) for k, v in shapes_dict.items()}
+    shapes_arr = np.array([y for x in shapes_dict.values() for y in x])
+    shape_slices = np.cumsum(np.array([len(v) for v in shapes_dict.values()])).tolist()
+    shape_slices = [slice(i, j) for i, j in zip([0, *shape_slices], shape_slices, strict=False)]
+    return shapes_arr, shape_slices
+
+
 def solve_mip(region: np.ndarray, shapes_arr: np.ndarray, shape_slices: list[slice]) -> int:
     """Solve a MIP to determine whether a region is feasible.
 
@@ -67,47 +132,9 @@ def solve_mip(region: np.ndarray, shapes_arr: np.ndarray, shape_slices: list[sli
             raise Exception(msg)
 
 
-def p12a(input_stream: TextIOBase) -> int:
-    """Find how many regions can fit their listed presents.
-
-    Overkill (but fun) approach using integer programming!
-    """
-    shapes, regions = parse_input(input_stream)
-
-    # add rotated and flipped versions
-    shapes_dict = {}
-    for i, shape in enumerate(shapes):
-        shapes_dict[i] = set()
-        for _ in range(4):
-            shape_tuple = tuple(tuple(row) for row in shape.tolist())
-            shapes_dict[i].add(shape_tuple)
-            # flipped
-            shape_tuple = tuple(tuple(row) for row in shape[::-1, :].tolist())
-            shapes_dict[i].add(shape_tuple)
-            # next rotation
-            shape = shape.T[::-1, :]  # noqa: PLW2901
-    shapes_dict = {k: sorted(v) for k, v in shapes_dict.items()}
-    shapes_arr = np.array([y for x in shapes_dict.values() for y in x])
-    shape_slices = np.cumsum(np.array([len(v) for v in shapes_dict.values()])).tolist()
-    shape_slices = [slice(i, j) for i, j in zip([0, *shape_slices], shape_slices, strict=False)]
-
-    success = 0
-    for region in tqdm(regions):
-        print(f"starting {region}...")
-        success += solve_mip(region, shapes_arr, shape_slices)
-
-    return success
-
-
-def p12b(input_stream: TextIOBase) -> int:
-    """TBD."""
-    print(parse_input(input_stream))
-    return 0
-
-
 if __name__ == "__main__":
     from pathlib import Path
 
     puzzle_input_path = Path("data") / "aoc25" / "p12.txt"
     with puzzle_input_path.open() as f:
-        print(p12a(f))
+        print(p12a_mip(f))
